d73251bf805697c8edf01b367853b1e85cc4dc6b
Lin Soong <linsoong@gmail.com>
2020-04-27 17:04:14 +0800

    20200427

---
+++ src/com/linsoong/leetcode/Q001_TwoNum.java
@@ -0,0 +1,66 @@
+package com.linsoong.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+public class Q001_TwoNum {
+
+	public static void main(String[] args) {
+		int[] nums = new int[] {2,7,11,15};
+
+	}
+	
+	public int[] twoSum(int[] nums, int target) {
+		
+		for(int i=0; i<nums.length; i++) {
+			for(int j=i+1; j<nums.length; j++) {
+				if(nums[i] + nums[j] == target) {
+					return new int[] {i, j};
+				}
+			}
+		}
+		
+        throw new IllegalArgumentException("no solution.");
+    }
+	
+	public int[] twoSum2(int[] nums, int target) {
+		Map<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
+		for(int i=0; i<nums.length; i++) {
+			numsMap.put(nums[i], i);
+		}
+		
+		for(int i=0; i<nums.length; i++) {
+			if(numsMap.containsKey(target-nums[i]) && numsMap.get(target-nums[i]) != i) {
+				return new int[] {i, numsMap.get(target-nums[i])};
+			}
+		}
+		
+        throw new IllegalArgumentException("no solution.");
+    }
+	
+	public int[] twoSum3(int[] nums, int target) {
+		Map<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
+		
+		for(int i=0; i<nums.length; i++) {
+			if(numsMap.containsKey(target-nums[i]) && numsMap.get(target-nums[i]) != i) {
+				return new int[] {i, numsMap.get(target-nums[i])};
+			}
+			numsMap.put(nums[i], i);
+		}
+		
+        throw new IllegalArgumentException("no solution.");
+    }
+
+}
---
+++ src/com/linsoong/leetcode/Q002_AddTwoNumbers.java
@@ -0,0 +1,69 @@
+package com.linsoong.leetcode;
+
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+ */
+public class Q002_AddTwoNumbers {
+
+	public static void main(String[] args) {
+
+		Q002_AddTwoNumbers q2 = new Q002_AddTwoNumbers();
+
+		q2.VerifyResult();
+	}
+	
+	public void VerifyResult() {
+		ListNode l1 = new ListNode(5);
+		ListNode l2 = new ListNode(5);
+		ListNode res = addTwoNumbers(l1, l2);
+		
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode res = null;
+		ListNode temp = null;
+		int count = 0;
+		while (count == 1 || l1 != null || l2 != null) {
+			if(res == null) {
+				res = new ListNode(((l1==null?0:l1.val) + (l2==null?0:l2.val) + count) % 10);
+				temp = res;
+			}else {
+				temp.next = new ListNode(((l1==null?0:l1.val) + (l2==null?0:l2.val) + count) % 10);
+				temp = temp.next;
+			}
+
+			if (((l1==null?0:l1.val) + (l2==null?0:l2.val) + count) >= 10) {
+				count = 1;
+			} else {
+				count = 0;
+			}
+			
+			if(l1 != null) {
+				l1 = l1.next;
+			}
+			if(l2 != null) {
+				l2 = l2.next;
+			}
+		}
+
+		return res;
+	}
+
+	private class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+}
